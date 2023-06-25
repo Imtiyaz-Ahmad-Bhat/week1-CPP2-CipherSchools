f6b26126b0d27822900990a5b5b9188c191c8e25
Imtiyaz-Ahmad-Bhat <117012991+Imtiyaz-Ahmad-Bhat@users.noreply.github.com>
2023-06-25 17:45:15 +0530

    Add files via upload

---
+++ Lecture1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int n1,int n2){
+    return n1+n2;
+}
+int main(){
+// std::cout<<"Hello world"; //hello world first test program//
+//cout<<"Hello world";
+//int,char,string,float,double
+  int n1,n2;
+    cin>>n1>>n2;
+    cout<<"The sum of n1 and n2 is : "<<sum(n1,n2);
+  return 0;
+}
+
