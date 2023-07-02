6ff0c5e5c5339b000baf19bc947c5497bdff61d1
Imtiyaz-Ahmad-Bhat <117012991+Imtiyaz-Ahmad-Bhat@users.noreply.github.com>
2023-07-02 22:39:59 +0530

    Add files via upload

---
+++ Lecture4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  //int a=10;
+  //int *p=&a;
+  int b1 = 10;
+  int&a= b1;
+cout<< b1 << endl;
+cout<< &a << endl;
+ // cout<<*(&a)<<endl;//dereferencing of the variable//
+  //cout<<*p<<endl;
+    // cout<<sizeof(p)<<endl;
+   // ++*p;
+     //cout<<*p<<endl;
+    // printf("%d",sizeof((a));
+    //char ch = 'a';
+   // char *pch=&ch;
+    //if(sizeof(pch)==sizeof(p)){
+      //  cout<< "hey"<<endl;
+   // }else{
+       // cout<<"hii"<<endl;
+   // }
+
+  return 0;
+}
